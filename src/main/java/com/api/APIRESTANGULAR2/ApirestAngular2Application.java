63a0f91af6bed27778aaae0065cf13a2340d42d6
VieriElCrack1 <santosvieri9@gmail.com>
2024-04-18 10:36:56 -0500

    api rest angular

---
+++ src/main/java/com/api/APIRESTANGULAR2/ApirestAngular2Application.java
@@ -0,0 +1,13 @@
+package com.api.APIRESTANGULAR2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestAngular2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestAngular2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/APIRESTANGULAR2/ApirestAngular2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.APIRESTANGULAR2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestAngular2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
